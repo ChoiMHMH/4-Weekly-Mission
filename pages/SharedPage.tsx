3444f344d9e283eb8e7b0cba92b26435201e0826
minho <kasamin98@naver.com>
2024-03-31 18:18:01 +0900

    refactor: 스타일 파일 위치 /style로 이동, default export로 변경(FolderPage.tsx, SharedPage.tsx)

--- pages/SharedPage.tsx
+++ pages/SharedPage.tsx
@@ -7,7 +7,7 @@ import { ReadOnlyCard } from "link/ui-read-only-card";
 import { SearchBar } from "link/ui-search-bar";
 import { useSearchLink } from "link/util-search-link/useSearchLink";
 
-export const SharedPage = () => {
+const SharedPage = () => {
   const { data } = useGetFolder();
   const { profileImage, ownerName, folderName, links } = data || {};
   const { searchValue, handleChange, handleCloseClick, result } =
@@ -41,3 +41,4 @@ export const SharedPage = () => {
     </Layout>
   );
 };
+export default SharedPage;
